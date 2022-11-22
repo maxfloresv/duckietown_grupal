23b60433b09639d20e69b1bc4e1683014f3b3628
Pichiuwu <111795826+Pichiuwu@users.noreply.github.com>
2022-11-22 12:58:22 -0300

    feat: archivo listo

--- voice2text.py
+++ voice2text.py
@@ -1,67 +1,82 @@
-
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Joy
 import cv2
 import time
-
 import speech_recognition as sr
+import sys
 
-global ejecutar
-ejecutar = True
+# Mientras run=True, corre el programa
+run = True
+active = False
 
 class Template(object):
 	def __init__(self, args):
 		super(Template, self).__init__()
 		self.args = args
-		
+
 		# Publisher de voz
 		# latch=True para publicar mensajes perdidos
 		# https://stackoverflow.com/a/60661094
 		self.pub = rospy.Publisher("/duckiebot/voz/v2t", String, queue_size=1, latch=True)
-		
-		rospy.Subscriber("/duckiebot/joy", Joy, self.callback)
+
+		# Subscribers
+		rospy.Subscriber("/duckiebot/joy", Joy, self.callback_control)
 
 		# Programa de voz
 		self.r = sr.Recognizer()
 
-	def callback(self, msg):
-		buttons = list(msg.buttons)
-		
-		A = buttons[0]
+		self.A = 0
+		self.X = 0
 
-		# Ejecutar el microfono
-		if A == 1:
+	def callback(self):
+		# Modifico los valores globales
+		global run, active
+
+		if self.X == 1:
+		    run = False
+		    return
+
+		# Ejecutar el microfono solo si no esta activo (evita lag)
+		if self.A == 1 and not active:
+			active = True
 			with sr.Microphone() as source:
-				print("Quack quack...") # que lo diga
+				print("Quack quack...")  # que lo diga
 				audio = self.r.listen(source, None, 1.5)
-				
+
 				try:
 					text = self.r.recognize_google(audio, language='es-ES')
 					print("Lo quack dijiste fue:", str(text))
 					msg = String()
 					msg.data = str(text)
 					self.pub.publish(msg)
-					
 				except Exception as e:
 					print("No quackche", str(e))
+
+				active = False
+
+	def callback_control(self, msg):
+		buttons = list(msg.buttons)
+
+		self.A = buttons[0]
+		self.X = buttons[2]
 		
+
 def main():
 	# Nodo local del PC
 	rospy.init_node("pc")
-	
+
 	print("Funcionando!")
-	
+
 	obj = Template('args')
-		
-	rospy.spin()
-	# Ejecutar programa cada 10ms
-	"""
+
 	rate = rospy.Rate(10)
-	while (not rospy.is_shutdown()) and ejecutar:
-		print(True)
+	while not rospy.is_shutdown():
+		if run == False:
+			sys.exit()
+	    
 		obj.callback()
-		rate.sleep()"""
+		rate.sleep()
 
 if __name__ == '__main__':
 	main()
