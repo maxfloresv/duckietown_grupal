9f3b6ca81eb336a711e0b7db1f827894fb2d5b51
Máximo Flores Valenzuela <maximo.flores@ug.uchile.cl>
2022-12-14 18:45:02 -0300

    listo

--- voice2text.py
+++ voice2text.py
@@ -1,10 +1,14 @@
+
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Empty
 from sensor_msgs.msg import Joy
 import cv2
 import time
 import speech_recognition as sr
 import sys
+import wikipediaapi
+
+wiki = wikipediaapi.Wikipedia('es')
 
 # Mientras run=True, corre el programa
 run = True
@@ -17,7 +21,8 @@ class Template(object):
 
 		# Publisher de voz
 		self.pub_voz = rospy.Publisher("/duckiebot/voz/v2t", String, queue_size=10)
-		self.pub_tiempo = rospy.Publisher("/duckiebot/voz/tiempo", String, queue_size=1)
+		self.pub_tiempo = rospy.Publisher("/duckiebot/voz/tiempo", String, queue_size=10)
+		self.pub_wiki = rospy.Publisher("/duckiebot/voz/wiki", String, queue_size=10)
 
 		# Subscribers
 		rospy.Subscriber("/duckiebot/joy", Joy, self.callback_control)
@@ -29,7 +34,7 @@ class Template(object):
 		self.A = 0
 		self.X = 0
 
-	def callback(self, auto=False):
+	def callback(self, instruccion=None, auto=False):
 		# Modifico los valores globales
 		global run, active
 
@@ -41,22 +46,34 @@ class Template(object):
 		if (self.A == 1 and not active) or auto:
 			active = True
 			with sr.Microphone() as source:
-				print("Quack quack...")  # que lo diga
+				if not auto:
+					print("Quack quack...")
+				else:
+					print("Quack %s..." % instruccion)
 				audio = self.r.listen(source, None, 1.5)
 
+				msg = String()
+				
 				try:
 					text = self.r.recognize_google(audio, language='es-ES')
 					print("Lo quack dijiste fue:", str(text))
-					msg = String()
 					msg.data = str(text)
 					
-					# Si se llama desde el programa, es tiempo (en segundos)
-					if auto:
-						self.pub_tiempo.publish(msg)
-					# Sino, es probablemente una instruccion
+					if auto and instruccion != None:
+						if instruccion == "tiempo":
+							self.pub_tiempo.publish(msg)
+						elif instruccion == "buscar":
+							pagina = wiki.page(msg.data)
+							resumen = pagina.summary[0:60]
+							msg.data = resumen
+							self.pub_wiki.publish(msg)
 					else:
 						self.pub_voz.publish(msg)
 				except Exception as e:
+					# Si no capta la instruccion, debe volver a pedir input
+					if instruccion == "tiempo":
+						self.pub_tiempo.publish(msg)
+				
 					print("No quackche", str(e))
 
 				active = False
@@ -68,8 +85,9 @@ class Template(object):
 		self.X = buttons[2]
 		
 	def callback_req(self, msg):
-		self.callback(True)
+		inst = msg.data
 		
+		self.callback(inst, True)	
 
 def main():
 	# Nodo local del PC
